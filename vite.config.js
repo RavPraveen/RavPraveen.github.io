11409fb2bc8891e0b6ac6f0cbb3d96ad9e586ac7
RP-Samarasekara <ravinpraveen416@gmail.com>
2026-08-02 01:33:45 +0530

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const isProd = process.env.NODE_ENV === 'production'
-
 export default defineConfig({
-  base: isProd ? '/RavPraveen.github.io/' : '/',
+  base: '/',
   plugins: [react()]
 })
